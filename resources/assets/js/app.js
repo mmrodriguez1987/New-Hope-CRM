83eb71c80e26baa6a851dc56d8fa5ca8632376d4
Michael Rodriguez <info@nissienterprise.com>
2019-02-26 15:00:52 -0500

    configuring the Vue App
     Laravel 5.7.28 Stable

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,5 @@
+
+import './polyfill'
 import router from './router';
 import store from './store'
 import App from './components/App'
@@ -18,8 +20,5 @@ const app = new Vue({
   el: '#app',
   router,
   store,
-  template: '<App/>',
-  components: {
-    App
-  }
+  render: h => h(App),
 })
