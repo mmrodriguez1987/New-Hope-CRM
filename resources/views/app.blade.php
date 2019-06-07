ad0c4195efae38ee5c7806548acc8cb736b2a7c8
Michael Rodriguez <info@nissienterprise.com>
2019-06-07 01:20:58 -0400

    Core-ui standarization

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="UTF-8">
+        <title>New Hope Dasboard</title>        
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {!! Html::style('css/app.css') !!}
+    <body>
+        <div id="app">  
+            <router-view></router-view>
+        {!! Html::script('js/lang.js') !!}
+        {!! Html::script('js/app.js') !!}
+    </body>
+</html>
