86ff2157f199ebb055893eaf6aacc26f26f19a45
Rodriguez <mmrodriguez1987@gmail.com>
2019-06-13 00:15:15 -0400

    Dashboard Ready

--- resources/assets/js/_nav.js
+++ resources/assets/js/_nav.js
@@ -9,25 +9,7 @@ export default {
         text: 'NEW'
       }
     },
-    {
-      title: true,
-      name: 'Theme',
-      class: '',
-      wrapper: {
-        element: '',
-        attributes: {}
-      }
-    },
-    {
-      name: 'Colors',
-      url: '/theme/colors',
-      icon: 'icon-drop'
-    },
-    {
-      name: 'Typography',
-      url: '/theme/typography',
-      icon: 'icon-pencil'
-    },
+
     {
       title: true,
       name: 'Components',
--- routes/web.php
+++ routes/web.php
@@ -5,57 +5,52 @@
 
 Route::get('/', 'Admin\DashController@index')->name('home');
 
-// Route::prefix('admin')->middleware(['auth'])->group(function () {
+Route::prefix('admin')->middleware(['auth'])->group(function () {
 
-//     //Persons
-//     Route::get('person', 'PersonController@index')->name('api.person.index');   
-//     Route::post('person', 'PersonController@store')->name('api.person.store');
-//     Route::put('person/{id}', 'PersonController@update')->name('api.person.update');
-//     Route::delete('person/{id}', 'PersonController@delete')->name('api.person.delete');
-//     Route::get('personList', 'PersonController@list')->name('api.person.list');
+     //Persons
+    Route::get('person', 'PersonController@index')->name('api.person.index');   
+    Route::post('person', 'PersonController@store')->name('api.person.store');
+    Route::put('person/{id}', 'PersonController@update')->name('api.person.update');
+    Route::delete('person/{id}', 'PersonController@delete')->name('api.person.delete');
+    Route::get('personList', 'PersonController@list')->name('api.person.list');
 
-//     //Professions
-//     Route::get('profession', 'ProfessionController@index')->name('api.profession.index');   
-//     Route::post('profession', 'ProfessionController@store')->name('api.profession.store');
-//     Route::put('profession/{id}', 'ProfessionController@update')->name('api.profession.update');
-//     Route::delete('profession/{id}', 'ProfessionController@delete')->name('api.profession.delete');
-//     Route::get('professionList', 'ProfessionController@list')->name('api.profession.list');
+    //Professions
+    Route::get('profession', 'ProfessionController@index')->name('api.profession.index');   
+    Route::post('profession', 'ProfessionController@store')->name('api.profession.store');
+    Route::put('profession/{id}', 'ProfessionController@update')->name('api.profession.update');
+    Route::delete('profession/{id}', 'ProfessionController@delete')->name('api.profession.delete');
+    Route::get('professionList', 'ProfessionController@list')->name('api.profession.list');
 
-//     //Postions
-//     Route::get('position', 'PositionController@index')->name('api.position.index');
-//     Route::post('position', 'PositionController@store')->name('api.position.store');
-//     Route::put('position/{id}', 'PositionController@update')->name('api.position.update');
-//     Route::delete('position/{id}','PositionController@delete')->name('api.position.delete');
-//     Route::get('positionList', 'PositionController@positionList')->name('api.position.list');
+    //Postions
+    Route::get('position', 'PositionController@index')->name('api.position.index');
+    Route::post('position', 'PositionController@store')->name('api.position.store');
+    Route::put('position/{id}', 'PositionController@update')->name('api.position.update');
+    Route::delete('position/{id}','PositionController@delete')->name('api.position.delete');
+    Route::get('positionList', 'PositionController@positionList')->name('api.position.list');
 
-//     //Person types
-//     Route::get('persontype', 'PersontypeController@index')->name('api.persontype.index');
-//     Route::post('persontype', 'PersontypeController@store')->name('api.persontype.store');
-//     Route::put('persontype/{id}', 'PersontypeController@update')->name('api.persontype.update');
-//     Route::delete('persontype/{id}', 'PersontypeController@delete')->name('api.persontype.delete');
-//     Route::get('persontypeList','PersontypeController@list')->name('api.persontype.list');
-
-    
-// });
+    //Person types
+    Route::get('persontype', 'PersontypeController@index')->name('api.persontype.index');
+    Route::post('persontype', 'PersontypeController@store')->name('api.persontype.store');
+    Route::put('persontype/{id}', 'PersontypeController@update')->name('api.persontype.update');
+    Route::delete('persontype/{id}', 'PersontypeController@delete')->name('api.persontype.delete');
+    Route::get('persontypeList','PersontypeController@list')->name('api.persontype.list');
+});
 
 
-// Localization
-// Route::get('/js/lang.js', function () {
-//     $strings = Cache::rememberForever('lang.js', function () {
-//         $lang = config('app.locale');
+    // Localization
+    Route::get('/js/lang.js', function () {
+        $strings = Cache::rememberForever('lang.js', function () {
+            $lang = config('app.locale');
+            $files = glob(resource_path('lang/' . $lang . '/*.php'));
+            $strings = [];
+            foreach ($files as $file) {
+                $name = basename($file, '.php');
+                $strings[$name] = require $file;
+            }
+            return $strings;
+        });
 
-//         $files   = glob(resource_path('lang/' . $lang . '/*.php'));
-//         $strings = [];
-
-//         foreach ($files as $file) {
-//             $name           = basename($file, '.php');
-//             $strings[$name] = require $file;
-//         }
-
-//         return $strings;
-//     });
-
-//     header('Content-Type: text/javascript');
-//     echo('window.i18n = ' . json_encode($strings) . ';');
-//     exit();
-// })->name('assets.lang');
+        header('Content-Type: text/javascript');
+        echo('window.i18n = ' . json_encode($strings) . ';');
+        exit();
+    })->name('assets.lang');
